cec2318d89225c18287b87d17ee2517b00b8ef1a
Anil Kulkarni <anilkulkarni@fb.com>
2018-10-03 11:46:11 -0700

    Working splashscreen

--- assets/js/revealingPicture.js
+++ assets/js/revealingPicture.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 
-const RevealingPicture = (props) => (
-  <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-    <div class="flipper">
-      <div class="front">
+const RevealingPicture = (props) => {
+  const styles = "flip-container" + (props.show ? " hover" : "");
+  return (<div className={styles}>
+    <div className="flipper">
+      <div className="front">
       </div>
-      <div class="back">
+      <div className="back">
         <div className="revealing">
             <img src={props.source}></img>
         </div>
       </div>
     </div>
-  </div>
-);
+  </div>);
+};
 export default RevealingPicture;
--- assets/js/splash.js
+++ assets/js/splash.js
@@ -2,23 +2,52 @@ import React, { Component } from 'react';
 import RevealingPicture from './revealingPicture';
 
 class Splash extends Component {
+  constructor(props) {
+    super(props);
+    this.interval = null;
+    this.state = {
+      visiblePictures: 0,
+    }
+    this.incrementPictures = this.incrementPictures.bind(this);
+  }
+
+  componentDidMount() {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.incrementPictures, 500);
+  }
+
+  incrementPictures() {
+    this.setState((state) => {
+      if (state.visiblePictures <= 4) {
+        state = {
+          visiblePictures: state.visiblePictures + 1,
+        };
+      } else {
+        clearInterval(this.interval);
+        this.interval = null;
+      }
+      return state;
+    });
+  }
+
   render() {
+    const v = this.state.visiblePictures;
     return (
       <div id="splash">
         <div className="row">
           <div className="quad">
-            <RevealingPicture source="/images/1.jpg"/>
+            <RevealingPicture source="/images/1.jpg" show={v >= 1} />
           </div>
           <div className="quad">
-            <RevealingPicture source="/images/2.jpg"/>
+            <RevealingPicture source="/images/2.jpg"  show={v >= 2}/>
           </div>
         </div>
         <div className="row">
           <div className="quad">
-            <RevealingPicture source="/images/3.jpg"/>
+            <RevealingPicture source="/images/3.jpg"  show={v >= 3}/>
           </div>
           <div className="quad">
-            <RevealingPicture source="/images/4.jpg"/>
+            <RevealingPicture source="/images/4.jpg"  show={v >= 4}/>
           </div>
         </div>
       </div>
